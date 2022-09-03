33ed99b87f12105290b3548ca872b3bb7fc57f69
Kaicho Sun <kaichaosuna@gmail.com>
2022-09-03 15:35:08 +0800

    poe benchmark all

--- pallets/poe/src/benchmarking.rs
+++ pallets/poe/src/benchmarking.rs
@@ -1,5 +1,5 @@
 use crate::*;
-use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_benchmarking::{benchmarks, whitelisted_caller, account};
 use frame_system::RawOrigin;
 
 benchmarks! {
@@ -8,6 +8,21 @@ benchmarks! {
 		let claim = vec![0; d as usize];
 		let caller: T::AccountId = whitelisted_caller();
 	}: _(RawOrigin::Signed(caller), claim)
+
+	revoke_claim {
+		let d in 0 .. T::MaxClaimLength::get();
+		let claim = vec![0; d as usize];
+		let caller: T::AccountId = whitelisted_caller();
+		assert!(Pallet::<T>::create_claim(RawOrigin::Signed(caller.clone()).into(), claim.clone()).is_ok());
+	}: _(RawOrigin::Signed(caller), claim)
+
+	transfer_claim {
+		let d in 0 .. T::MaxClaimLength::get();
+		let claim = vec![0; d as usize];
+		let caller: T::AccountId = whitelisted_caller();
+		let target: T::AccountId = account("target", 0, 0);
+		assert!(Pallet::<T>::create_claim(RawOrigin::Signed(caller.clone()).into(), claim.clone()).is_ok());
+	}: _(RawOrigin::Signed(caller), claim, target)
 
 	impl_benchmark_test_suite!(PoeModule, crate::mock::new_test_ext(), crate::mock::Test);
 }
--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -83,7 +83,7 @@ pub mod pallet {
 			Ok(().into())
 		}
 
-		#[pallet::weight(0)]
+		#[pallet::weight(T::WeightInfo::revoke_claim(claim.len() as u32))]
 		pub fn revoke_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResultWithPostInfo {
 			let sender = ensure_signed(origin)?;
 
@@ -100,7 +100,7 @@ pub mod pallet {
 			Ok(().into())
 		}
 
-		#[pallet::weight(0)]
+		#[pallet::weight(T::WeightInfo::transfer_claim(claim.len() as u32))]
 		pub fn transfer_claim(
 			origin: OriginFor<T>,
 			claim: Vec<u8>,
